6e8d4bdda998d8e4dbad88491ee9ba9c6a8a88de
YOUR_NAME <YOUR_EMAIL>
2022-12-24 19:34:31 +0300

    add makemigrations and migrate

--- src/djangoflix/videos/models.py
+++ src/djangoflix/videos/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Video(models.Model):
+  title = models.CharField()
+  description = models.TextField()
+  slug = models.SlugField(blank=True, null=True) # 'this-is-my-video'
+  video_id = models.CharField()
+  # timestamp
+  # updated
+  # state 
+  # publish_timestamp
+  
+  
